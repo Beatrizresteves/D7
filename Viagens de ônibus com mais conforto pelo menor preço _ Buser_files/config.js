2f9147cd1aa9d3f28807db14bd899f34115d6277
Beatriz Esteves <beatrizramalho.esteves@gmail.com>
2022-05-16 18:22:01 -0300

    d7

---
+++ Viagens de ônibus com mais conforto pelo menor preço _ Buser_files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C5RIED4247C0IGAK1J90").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C5RIED4247C0IGAK1J90").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C5RIED4247C0IGAK1J90").setPixelInfo({status: 0, name: "Buser", advertiserID: "7021133387033247746", setupMode: 1, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("buser.com.br");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
